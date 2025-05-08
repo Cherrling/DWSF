89ee9e1989593ea5ce64c39d507a092419bd21bf
Cherrling <cherrling2021@outlook.com>
2025-05-09 05:08:29 +0800

    add test_encoder_decoder
    (cherry picked from commit 80b4479722ef1ded5943bb74066e131f242d8e70)

---
+++ test_encoder_decoder.sh
@@ -0,0 +1,7 @@
+python test_encoder_decoder.py \
+    --model_path /home/cherrling/code/DWSF/genimgpth \
+    --image_dir /home/cherrling/code/DWSF/eval_imgs \
+    --output_dir ./test_results \
+    --seg_model ./seg_99.pth \
+    --use_crc \
+    --visualize
